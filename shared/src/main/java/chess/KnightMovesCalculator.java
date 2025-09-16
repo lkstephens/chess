0081321a6c9c9176c21f177c1efbf9114dfd2bdc
Landon Stephens <lkstephens01@gmail.com>
2025-09-16 14:27:20 -0600

    impl(phase0) Knight Moves Pass

--- shared/src/main/java/chess/KnightMovesCalculator.java
+++ shared/src/main/java/chess/KnightMovesCalculator.java
@@ -1,11 +1,215 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 
 public class KnightMovesCalculator implements PieceMovesCalculator {
     @Override
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        return List.of();
+        int row = myPosition.getRow();
+        int col = myPosition.getColumn();
+        ChessPiece myPiece = board.getPiece(myPosition);
+        ChessGame.TeamColor myTeamColor = myPiece.getTeamColor();
+        Collection<ChessMove> knightMoves = new ArrayList<>();
+
+        // Up-Right
+        row = row+2;
+        col++;
+        if (row < 9 && col < 9) {
+            ChessPosition curr_pos = new ChessPosition(row, col);
+            ChessPiece curr_piece = board.getPiece(curr_pos);
+
+            // Position is not occupied by a piece
+            if (curr_piece == null) {
+
+                knightMoves.add(new ChessMove(myPosition, curr_pos, null));
+
+                // Position is occupied
+            } else {
+                ChessGame.TeamColor curr_team_color = curr_piece.getTeamColor();
+                // Capture
+                if (myTeamColor != curr_team_color) {
+                    knightMoves.add(new ChessMove(myPosition, curr_pos, null));
+                }
+            }
+        }
+
+        row = myPosition.getRow();
+        col = myPosition.getColumn();
+
+        // Right-Up
+        row++;
+        col = col+2;
+        if (row < 9 && col < 9) {
+            ChessPosition curr_pos = new ChessPosition(row, col);
+            ChessPiece curr_piece = board.getPiece(curr_pos);
+
+            // Position is not occupied by a piece
+            if (curr_piece == null) {
+
+                knightMoves.add(new ChessMove(myPosition, curr_pos, null));
+
+                // Position is occupied
+            } else {
+                ChessGame.TeamColor curr_team_color = curr_piece.getTeamColor();
+                // Capture
+                if (myTeamColor != curr_team_color) {
+                    knightMoves.add(new ChessMove(myPosition, curr_pos, null));
+                }
+            }
+        }
+
+        row = myPosition.getRow();
+        col = myPosition.getColumn();
+
+        // Right-Down
+        row--;
+        col = col+2;
+        if (row > 0 && col < 9) {
+            ChessPosition curr_pos = new ChessPosition(row, col);
+            ChessPiece curr_piece = board.getPiece(curr_pos);
+
+            // Position is not occupied by a piece
+            if (curr_piece == null) {
+
+                knightMoves.add(new ChessMove(myPosition, curr_pos, null));
+
+                // Position is occupied
+            } else {
+                ChessGame.TeamColor curr_team_color = curr_piece.getTeamColor();
+                // Capture
+                if (myTeamColor != curr_team_color) {
+                    knightMoves.add(new ChessMove(myPosition, curr_pos, null));
+                }
+            }
+        }
+
+        row = myPosition.getRow();
+        col = myPosition.getColumn();
+
+        // Down-Right
+        row = row-2;
+        col++;
+        if (row > 0 && col < 9) {
+            ChessPosition curr_pos = new ChessPosition(row, col);
+            ChessPiece curr_piece = board.getPiece(curr_pos);
+
+            // Position is not occupied by a piece
+            if (curr_piece == null) {
+
+                knightMoves.add(new ChessMove(myPosition, curr_pos, null));
+
+            // Position is occupied
+            } else {
+                ChessGame.TeamColor curr_team_color = curr_piece.getTeamColor();
+                // Capture
+                if (myTeamColor != curr_team_color) {
+                    knightMoves.add(new ChessMove(myPosition, curr_pos, null));
+                }
+            }
+        }
+
+        row = myPosition.getRow();
+        col = myPosition.getColumn();
+
+        // Down-Left
+        row = row-2;
+        col--;
+        if (row > 0 && col > 0) {
+            ChessPosition curr_pos = new ChessPosition(row, col);
+            ChessPiece curr_piece = board.getPiece(curr_pos);
+
+            // Position is not occupied by a piece
+            if (curr_piece == null) {
+
+                knightMoves.add(new ChessMove(myPosition, curr_pos, null));
+
+            // Position is occupied
+            } else {
+                ChessGame.TeamColor curr_team_color = curr_piece.getTeamColor();
+                // Capture
+                if (myTeamColor != curr_team_color) {
+                    knightMoves.add(new ChessMove(myPosition, curr_pos, null));
+                }
+            }
+        }
+
+        row = myPosition.getRow();
+        col = myPosition.getColumn();
+
+        // Left-Down
+        row--;
+        col = col-2;
+        if (row > 0 && col > 0) {
+            ChessPosition curr_pos = new ChessPosition(row, col);
+            ChessPiece curr_piece = board.getPiece(curr_pos);
+
+            // Position is not occupied by a piece
+            if (curr_piece == null) {
+
+                knightMoves.add(new ChessMove(myPosition, curr_pos, null));
+
+            // Position is occupied
+            } else {
+                ChessGame.TeamColor curr_team_color = curr_piece.getTeamColor();
+                // Capture
+                if (myTeamColor != curr_team_color) {
+                    knightMoves.add(new ChessMove(myPosition, curr_pos, null));
+                }
+            }
+        }
+
+
+        row = myPosition.getRow();
+        col = myPosition.getColumn();
+
+        // Left-Up
+        row++;
+        col = col-2;
+        if (row < 9 && col > 0) {
+            ChessPosition curr_pos = new ChessPosition(row, col);
+            ChessPiece curr_piece = board.getPiece(curr_pos);
+
+            // Position is not occupied by a piece
+            if (curr_piece == null) {
+
+                knightMoves.add(new ChessMove(myPosition, curr_pos, null));
+
+                // Position is occupied
+            } else {
+                ChessGame.TeamColor curr_team_color = curr_piece.getTeamColor();
+                // Capture
+                if (myTeamColor != curr_team_color) {
+                    knightMoves.add(new ChessMove(myPosition, curr_pos, null));
+                }
+            }
+        }
+
+        row = myPosition.getRow();
+        col = myPosition.getColumn();
+
+        // Up-Left
+        row = row+2;
+        col--;
+        if (row < 9 && col > 0) {
+            ChessPosition curr_pos = new ChessPosition(row, col);
+            ChessPiece curr_piece = board.getPiece(curr_pos);
+
+            // Position is not occupied by a piece
+            if (curr_piece == null) {
+
+                knightMoves.add(new ChessMove(myPosition, curr_pos, null));
+
+                // Position is occupied
+            } else {
+                ChessGame.TeamColor curr_team_color = curr_piece.getTeamColor();
+                // Capture
+                if (myTeamColor != curr_team_color) {
+                    knightMoves.add(new ChessMove(myPosition, curr_pos, null));
+                }
+            }
+        }
+
+        return knightMoves;
     }
 }
