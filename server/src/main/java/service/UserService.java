d7d31cd7d5b5886df8156830404140d184b3d680
Landon Stephens <lkstephens01@gmail.com>
2025-10-13 14:51:23 -0600

    impl(phase3) Started UserService, created bad request exception

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -1,7 +1,13 @@
 package service;
 
 public class UserService {
-    public RegisterResult register(RegisterRequest registerRequest) {
+    public RegisterResult register(RegisterRequest registerRequest) throws BadRequestException {
+        String username = registerRequest.username();
+        String password = registerRequest.password();
+        String email = registerRequest.email();
+
+
+
         return new RegisterResult(registerRequest.username(), registerRequest.password());
     }
     // public LoginResult login(LoginRequest loginRequest) {}
