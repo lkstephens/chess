cf3bb95816f2286a7b851aea0ee92f8ae746868e
Landon Stephens <lkstephens01@gmail.com>
2025-10-25 23:11:14 -0600

    impl(phase3) Played around with AuthData, added getAuth

--- server/src/main/java/dataaccess/SQLAuthDAO.java
+++ server/src/main/java/dataaccess/SQLAuthDAO.java
@@ -1,8 +1,19 @@
 package dataaccess;
 
+import com.google.gson.Gson;
 import model.AuthData;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class SQLAuthDAO implements AuthDAO{
+
+    public SQLAuthDAO () throws DataAccessException {
+        configureDatabase();
+    }
+
     @Override
     public void clear() {
 
@@ -15,11 +26,54 @@ public class SQLAuthDAO implements AuthDAO{
 
     @Override
     public AuthData getAuth(String authToken) throws DataAccessException {
+        try (Connection conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT authToken, username FROM pet WHERE authToken=?";
+            try (PreparedStatement ps = conn.prepareStatement(statement)) {
+                ps.setString(1, authToken);
+                try (ResultSet rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        return readAuthData(rs);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new DataAccessException("Error: ");
+        }
         return null;
     }
 
     @Override
     public void deleteAuth(AuthData authData) {
 
+    }
+
+    private AuthData readAuthData(ResultSet rs) throws SQLException {
+        var authToken = rs.getString("authToken");
+        var username = rs.getString("username");
+        return new AuthData(authToken, username);
+    }
+
+    private final String[] authCreateStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS  auth_data (
+              `authToken` char(36) NOT NULL,
+              `username` varchar(32) NOT NULL,
+              PRIMARY KEY (`authToken`)
+              ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """
+    };
+
+    // Creates table for AuthData
+    private void configureDatabase() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        try (Connection conn = DatabaseManager.getConnection()) {
+            for (String statement : authCreateStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException("Error: could not configure database", ex);
+        }
     }
 }
